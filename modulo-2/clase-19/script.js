56c25c0ef9da28dee22eb90afff46642fa7bffb5
Matias Marchetti <mhmarchetti96@gmail.com>
2023-08-14 22:09:53 -0300

    Se crea modulo 2

---
+++ modulo-2/clase-19/script.js
@@ -0,0 +1,29 @@
+// Función predeterminada y básica para ver errores y resultados en consola del navegador
+// console.log("Soy un resultado")
+
+// Función para ver resultados en el navegador en forma de ventana saliente
+// alert("Soy una alerta")
+
+
+// Tipos de datos en JavaScript
+// String: Cadena de caracteres
+// Ejemplo: "Matías"
+
+// Number: Tipos de datos de número
+
+// let primerNumero = 4;
+// let segundoNumero = 6;
+// let tercerNumero = 8;
+// let cuartoNumero = 9;
+
+// let resultadoMultiplicacion = primerNumero * segundoNumero;
+// let resultadoResta = tercerNumero - cuartoNumero;
+
+// console.log(resultadoMultiplicacion);
+// console.log(resultadoResta);
+
+let nombre = prompt("Ingrese su nombre");
+let apellido = prompt("Ingrese su apellido");
+let edad = parseInt(prompt("Ingrese su edad"));
+
+console.log(nombre + " " + apellido + " tiene " + edad + " años");
