541740e7e2f9ccc632c384fc346163de2e28364f
Matias Marchetti <mhmarchetti96@gmail.com>
2023-10-05 21:57:59 -0300

    Clases de la 26 a la 34

---
+++ modulo-2/clase-30/js/script.js
@@ -0,0 +1,9 @@
+// $(document).ready(function (){
+//     $("#saludosBtn").click(function (){
+//         alert("Hola mundo")
+//     })
+// })
+
+$("#saludosBtn").click(function (){
+    $("#miParrafo").text("Texto nuevo")
+})
