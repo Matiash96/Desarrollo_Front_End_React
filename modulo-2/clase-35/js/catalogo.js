ee0e5f8cacc2f397d5779989ae50d80ff66b6ab5
Matias Marchetti <mhmarchetti96@gmail.com>
2023-10-12 10:47:48 -0300

    Clase 35

---
+++ modulo-2/clase-35/js/catalogo.js
@@ -0,0 +1,38 @@
+const productos = [
+    {
+        nombre: 'Mesa',
+        destino: 'Comedor',
+        imagen: 'https://dummyimage.com/300x200/000/fff',
+        valor: 500
+    },
+    {
+        nombre: 'Escritorio',
+        destino: 'Oficina',
+        imagen: 'https://dummyimage.com/300x200/000/fff',
+        valor: 450
+    },
+    {
+        nombre: 'Silla',
+        destino: 'Comedor',
+        imagen: 'https://dummyimage.com/300x200/000/fff',
+        valor: 500
+    },
+    {
+        nombre: 'Mesa',
+        destino: 'Comedor',
+        imagen: 'https://dummyimage.com/300x200/000/fff',
+        valor: 500
+    },
+    {
+        nombre: 'Mesa',
+        destino: 'Comedor',
+        imagen: 'https://dummyimage.com/300x200/000/fff',
+        valor: 500
+    },
+    {
+        nombre: 'Mesa',
+        destino: 'Comedor',
+        imagen: 'https://dummyimage.com/300x200/000/fff',
+        valor: 500
+    },
+]
